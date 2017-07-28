ad1732894cdef11bd54dcfe45b8063d4eb7a8ac3
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2017-07-28 15:08:35 -0400

    Update CourseCreate.ps1

--- Concepts/AAPSWF/CourseCreate.ps1
+++ Concepts/AAPSWF/CourseCreate.ps1
@@ -47,37 +47,7 @@ catch {
 
     #Set ARM template parameters
 
-    $labVirtualNetworkName = "VNET-" + $ctxavslot
-
-    $newVMName = $ctxavslot
-
-    # Grab Date\Time add an hour to pass for Lab VMs expiration
-    $current = Get-Date -Date (Get-Date).AddHours(1) -Format o 
-    
-    $ucurrent = $current.split('.')[0] 
-
-    $expirationDate = $ucurrent + ".000Z"
-
-    #$expirationDate = "2017-07-03T17:00:00.000Z"
-    Parallel
-    {
-        # Login
-        #$AzureRMAccount = Add-AzureRmAccount -Credential $credentials -ServicePrincipal -TenantId "72f988bf-86f1-41af-91ab-2d7cd011db47"
-        New-AzureRmResourceGroupDeployment -ResourceGroupName "elearningRG540795" -TemplateUri "https://raw.githubusercontent.com/swiftsolves-msft/dtlelearnlab/master/Concepts/AAPSWF/azuredeploy1.json" -newVMName $newVMName -labVirtualNetworkName $labVirtualNetworkName -expirationDate $expirationDate
-        New-AzureRmResourceGroupDeployment -ResourceGroupName "elearningRG540795" -TemplateUri "https://raw.githubusercontent.com/swiftsolves-msft/dtlelearnlab/master/Concepts/AAPSWF/azuredeploy2.json"  -newVMNamestu $newVMName -labVirtualNetworkNamestu $labVirtualNetworkName -expirationDatestu $expirationDate
-    }
-
-    #Create additional Output
-
-    #Create a unique GUID for pair Key,Value Table
-    (InlineScript { 
-        $guid = New-Guid
-        Set-AzureRmAutomationVariable -AutomationAccountName "elearninglab" -ResourceGroupName "rgeLearningLabOrch" -Name "StudentGUID" -Value $guid -Encrypted $False
-
-        }
-    )
-
-    # generate a Random Password
+        # generate a Random Password
 
     #$ascii=$NULL;For ($a=33;$a –le 126;$a++) {$ascii+=,[char][byte]$a }
     $alphabet=$NULL;For ($a=65;$a –le 90;$a++) {$alphabet+=,[char][byte]$a }
@@ -97,7 +67,39 @@ catch {
     # Store a password
     $pass = GET-Temppassword –length 6 –sourcedata $alphabet
 
+    $pass 
+
     Set-AzureRmAutomationVariable -AutomationAccountName "elearninglab" -ResourceGroupName "rgeLearningLabOrch" -Name "StudentPassword" -Value $pass -Encrypted $False
+
+    $labVirtualNetworkName = "VNET-" + $ctxavslot
+
+    $newVMName = $ctxavslot
+
+    # Grab Date\Time add an hour to pass for Lab VMs expiration
+    $current = Get-Date -Date (Get-Date).AddHours(1) -Format o 
+    
+    $ucurrent = $current.split('.')[0] 
+
+    $expirationDate = $ucurrent + ".000Z"
+
+    #$expirationDate = "2017-07-03T17:00:00.000Z"
+    Parallel
+    {
+        # Login
+        #$AzureRMAccount = Add-AzureRmAccount -Credential $credentials -ServicePrincipal -TenantId "72f988bf-86f1-41af-91ab-2d7cd011db47"
+        New-AzureRmResourceGroupDeployment -ResourceGroupName "elearningRG540795" -TemplateUri "https://raw.githubusercontent.com/SwiftSolves-msft/dtlelearnlab/master/Concepts/AAPSWF/azuredeploy1.json" -password $pass -newVMName $newVMName -labVirtualNetworkName $labVirtualNetworkName -expirationDate $expirationDate
+        New-AzureRmResourceGroupDeployment -ResourceGroupName "elearningRG540795" -TemplateUri "https://raw.githubusercontent.com/SwiftSolves-msft/dtlelearnlab/master/Concepts/AAPSWF/azuredeploy2.json"  -password $pass -newVMNamestu $newVMName -labVirtualNetworkNamestu $labVirtualNetworkName -expirationDatestu $expirationDate
+    }
+
+    #Create additional Output
+
+    #Create a unique GUID for pair Key,Value Table
+    (InlineScript { 
+        $guid = New-Guid
+        Set-AzureRmAutomationVariable -AutomationAccountName "elearninglab" -ResourceGroupName "rgeLearningLabOrch" -Name "StudentGUID" -Value $guid -Encrypted $False
+
+        }
+    )
 
     Checkpoint-Workflow
 
@@ -128,6 +130,43 @@ catch {
 
     Rename-Item D:\temp\StudentLab.txt D:\temp\StudentLab.rdp
 
+    #generate attachment for email, ICA file to connect to Studentlab
+
+    $attach2 = New-Item D:\temp\StudentLabICA.txt -type file
+
+    Add-Content D:\temp\StudentLabICA.txt "[WFClient]"
+    Add-Content D:\temp\StudentLabICA.txt "`nVersion=2"
+    Add-Content D:\temp\StudentLabICA.txt "`nClientSessionID=0"
+    Add-Content D:\temp\StudentLabICA.txt "`n[ApplicationServers]"
+    Add-Content D:\temp\StudentLabICA.txt "`nStudentDesktop="
+    Add-Content D:\temp\StudentLabICA.txt "`n[StudentDesktop]"
+
+    $string1ica = "Address=" + $pipip + ":1494"
+    $string1ica
+    Add-Content D:\temp\StudentLabICA.txt $string1ica
+
+    Add-Content D:\temp\StudentLabICA.txt "`nInitialProgram="
+    Add-Content D:\temp\StudentLabICA.txt "`nClientAudio=Off"
+    Add-Content D:\temp\StudentLabICA.txt "`nAudioBandwidthLimit=1"
+    Add-Content D:\temp\StudentLabICA.txt "`nDefaultSoundType=0"
+    Add-Content D:\temp\StudentLabICA.txt "`nCompress=On"
+    Add-Content D:\temp\StudentLabICA.txt "`nTransportDriver=TCP/IP"
+    Add-Content D:\temp\StudentLabICA.txt "`nWinStationDriver=ICA 3.0"
+    Add-Content D:\temp\StudentLabICA.txt "`nSSLEnable=Off"
+    Add-Content D:\temp\StudentLabICA.txt "`nUsername=Administrator"
+    Add-Content D:\temp\StudentLabICA.txt "`nDomain=WORKSPACELAB"
+    Add-Content D:\temp\StudentLabICA.txt "`nClearPassword=Password1"
+    Add-Content D:\temp\StudentLabICA.txt "`n#DesiredHRES=1280"
+    Add-Content D:\temp\StudentLabICA.txt "`n#DesiredVRES=1024"
+    Add-Content D:\temp\StudentLabICA.txt "`nScreenPercent=80"
+    Add-Content D:\temp\StudentLabICA.txt "`nDesiredColor=8"
+    Add-Content D:\temp\StudentLabICA.txt "`nConnectionBar=1"
+    Add-Content D:\temp\StudentLabICA.txt "`n#CGPAddress=*:8080"
+
+    Rename-Item D:\temp\StudentLabICA.txt D:\temp\StudentLabICA.ica
+
+    $attach2 = "D:\temp\StudentLabICA.ica"
+
     # Generate Email
 
     $attach = "D:\temp\StudentLab.rdp"
@@ -136,7 +175,7 @@ catch {
 
     $credential = Get-AutomationPSCredential -Name 'SendGridCred'
 
-    Send-MailMessage -Attachments $attach -From no-reply@elearning.local -Subject "Student eLearning Lab Created" -To nate.swift@live.com -Body $body -smtpServer smtp.sendgrid.net -Port 587 -Credential $credential
+    Send-MailMessage -Attachments $attach,$attach2 -From no-reply@elearning.local -Subject "Student eLearning Lab Created" -To someone@somewhere.com -Body $body -smtpServer smtp.sendgrid.net -Port 587 -Credential $credential
 
     #$time = Get-Date
     Write-Output "Lab Deployed ."
